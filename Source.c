ca85154ad2d1e4b99242190f0c1a508033e46be8
Erik Martinez <61127579+ErikMtz99@users.noreply.github.com>
2022-11-08 19:25:35 -0600

    se leen arrays A y B

--- Source.c
+++ Source.c
@@ -30,11 +30,36 @@ void ContarMatrizA(int *count) {
 	}
 }
 
+void ContarMatrizB(int* count2) {
+	{
+		char const* const filename2 = "matrixB2500.txt";
+		FILE* fh2;
 
+		fopen_s(&fh2, filename2, "r");
+
+		if (!fh2) {
+			printf("\n unable to open : %s ", filename2);
+			return -1;
+		}
+
+		char line[500];
+
+		while (fgets(line, sizeof(line), fh2)) {
+			(*count2)++;
+			//float valor_convertido = atof(line);
+			//printf("float value : %.11f\n", valor_convertido);
+		}
+		fclose(fh2);
+		//printf("total num: %i", count);
+
+	}
+}
 int main(int argc, char* argv[]) {
 
 	FILE* fh;
+	FILE* fh2;
 	char const* const filename = "matrixA2500.txt";
+	char const* const filename2 = "matrixB2500.txt";
 	char line[500];
 	//Estos valores los debe de ingresar el usuario y hay que hacer validacion
 	int filasA = 50;
@@ -43,10 +68,13 @@ int main(int argc, char* argv[]) {
 	int columnasB = 50;
 
 	int count = 0;
+	int count2 = 0;
 
-	int total_esperado = filasA * columnasB;
+	int tam_matA = filasA * columnasA;
+	int tam_matB = filasB * columnasB;
+	int tam_matC = filasA * columnasB;
 
-	float* matB, * matC, * matA;
+	float* arrayB, * arrayC, * arrayA;
 
 	//Checar que se puedan multiplicar las matrices
 	if (columnasA != filasB) {
@@ -55,15 +83,27 @@ int main(int argc, char* argv[]) {
 
 	//Leer la matriz A para contar cuantos valores tiene
 	ContarMatrizA(&count);
-	printf("total num: %i", count);
+	printf("\ntotal num matriz A: %i", count);
 
-	//Checar que los tamanos coincidan
-	if (total_esperado != count) {
-		printf("\n total de valores en el archivo no coincide con tamaño de la matriz");
+	//Leer la matriz B para contar cuantos valores tiene
+	ContarMatrizB(&count2);
+	printf("\ntotal num matriz B: %i", count2);
+
+	//Checar que los tamanos coincidan de matriz A
+	if (tam_matA != count) {
+		printf("\n total de valores en el archivo no coincide con tamaño de la matriz A");
 	}
 
-	//Asignar memoria dinamica a array
-	matA = (float*)malloc(sizeof(float) * count + 1);
+	//Checar que los tamanos coincidan de matriz B
+	if (tam_matB != count2) {
+		printf("\n total de valores en el archivo no coincide con tamaño de la matriz B");
+	}
+
+	//Asignar memoria dinamica a arrays
+	arrayA = (float*)malloc(sizeof(float) * count);
+	arrayB = (float*)malloc(sizeof(float) * count2);
+	arrayC = (float*)malloc(sizeof(float) * tam_matC);
+
 
 	//*************************************************************************************************
 	//********************* Guardar valores de .txt en array matA *************************************
@@ -78,21 +118,46 @@ int main(int argc, char* argv[]) {
 	float valor_convertido2 = 0.0;
 	while (fgets(line, sizeof(line), fh)) {
 		valor_convertido2 = atof(line);
-		matA[i] = valor_convertido2;
+		arrayA[i] = valor_convertido2;
 		i++;
 	}
 
 	fclose(fh);
 	//*************************************************************************************************
 
-	//imprimir valores de array matA para comprobar que se guardaron bien.
-	for (int j = 0; j < count; j++) {
-		printf("\n %.11f ", matA[j]);
+	//********************* Guardar valores de .txt en array matB *************************************
+	fopen_s(&fh2, filename2, "r");
+
+	if (!fh2) {
+		printf("\n unable to open : %s ", filename2);
+		return -1;
 	}
 
-	//liberar espacio de memoria del malloc()
-	free(matA);
+	i = 0;
+	valor_convertido2 = 0.0;
+	while (fgets(line, sizeof(line), fh2)) {
+		valor_convertido2 = atof(line);
+		arrayB[i] = valor_convertido2;
+		i++;
+	}
 
+	fclose(fh2);
+	//*************************************************************************************************
+
+	//imprimir valores de array matA para comprobar que se guardaron bien.
+	//for (int j = 0; j < count; j++) {
+	//	printf("\n %.11f ", arrayA[j]);
+	//}
+
+	//imprimir valores de array matB para comprobar que se guardaron bien.
+	//for (int j = 0; j < count; j++) {
+	//	printf("\n %.11f ", arrayB[j]);
+	//}
+
+	//liberar espacio de memoria del malloc()
+	free(arrayA);
+	free(arrayB);
+	free(arrayC);
 	return 0;
 }
 
